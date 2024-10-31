21025a5d2d17fec270ccf3f3a6cce04f7a1f7fc8
Anwi-J <anwirjanga@gmail.com>
2024-10-31 18:51:32 -0400

    Add files via upload

---
+++ pose_detection.py
@@ -0,0 +1,22 @@
+import cv2
+import mediapipe as mp
+
+drawing = mp.solutions.drawing_utils
+pose = mp.solutions.pose
+p=pose.Pose()
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame,(600,600))
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = p.process(image)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        drawing.draw_landmarks(image, results.pose_landmarks,pose.POSE_CONNECTIONS)
+    cv2.imshow('pose tracking', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
